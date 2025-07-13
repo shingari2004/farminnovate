f896f3ae5d58a5f49103e49b352126c51b7e6711
shingari2004 <garvshingari2004@gmail.com>
2025-07-14 02:49:48 +0530

    pudh

--- components/Language.tsx
+++ components/Language.tsx
@@ -1,16 +1,18 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const LanguageInput = () => {
+
   return (
-    <div className="flex items-center">
-      <select>
-        <option value="en">EN</option>
-        <option value="hi">HI</option>
-        <option value="pun">PUN</option>
-      </select>
-    </div>
+      <div className="flex items-center">
+        <select  
+        >
+          <option value="en">EN</option>
+          <option value="hi">HI</option>
+          <option value="pun">PUN</option>  
+        </select>
+      </div>
   );
 };
 
